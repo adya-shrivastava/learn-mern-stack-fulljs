7c943a313177c49cce87ddb3c74309f9a7a4617d
adyaS <sweetadz12@gmail.com>
2020-12-22 16:24:07 -0500

    Navigating and Looking up a contest

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,11 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import Header from './Header';
-import ContestPreview from './ContestPreview';
+import ContestList from './ContestList';
+import Contest from './Contest';
+
+const pushState = (object, url) =>
+    window.history.pushState(object, '', url);
 
 class App extends React.Component {
     state = {
@@ -13,17 +17,36 @@ class App extends React.Component {
 
     componentWillUnmount() {
     }
-    
+
+    fetchContest = (contestId) => {
+        pushState(
+            { currentContestId: contestId },
+            `/contest/${contestId}`
+        );
+
+        // look up the contest
+        this.setState({
+            pageHeader: this.state.contests[contestId].contestName,
+            currentContestId: contestId
+        });
+    };
+
+    currentContent() {
+        if (this.state.currentContestId) {
+            return <Contest {...this.state.contests[this.state.currentContestId]} />;
+        }
+
+        return <ContestList
+            onContestClick={this.fetchContest}
+            contests={this.state.contests} />;
+    }
+
     render() {
         return (
             <div className='App'>
                 <Header message={this.state.pageHeader} />
-                <div>
-                    {this.state.contests.map(contest =>
-                        <ContestPreview key={contest.id} {...contest} />
-                    )}
+                {this.currentContent()}
 
-                </div>
             </div>
         );
     }
